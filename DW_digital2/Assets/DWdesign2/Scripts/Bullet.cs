b791dfc41f8fa8ea49da8c10f86805ac112d4dca
Pamela Payad <payadpamela@gmail.com>
2019-10-29 22:54:25 -0400

    turret
    turret time

---
+++ DW_digital2/Assets/DWdesign2/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+
+    public float bulletLife;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Kill();
+    }
+
+    public void Kill()
+    {
+        Destroy(gameObject, bulletLife);
+    }
+}
---
+++ DW_digital2/Assets/DWdesign2/Scripts/Turret.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    //turret
+    public float rotateSpeed;
+
+    //bullets
+    public Transform bulletSpawner;
+    public GameObject bulletPrefab;
+
+    public float bulletForce;
+    public float fireRate;
+
+    private void Start()
+    {
+        InvokeRepeating("ShootBullets", 0f, fireRate);
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        RotatePosition();
+    }
+
+    void RotatePosition()
+    {
+        transform.Rotate(Vector3.down * rotateSpeed * Time.deltaTime);
+    }
+
+    void ShootBullets()
+    {
+        GameObject bullet;
+        bullet = Instantiate(bulletPrefab, bulletSpawner.position, Quaternion.identity) as GameObject;
+
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawner.right * bulletForce, ForceMode.Impulse);
+    }
+
+}
