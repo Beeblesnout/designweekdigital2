5f1da8aa0d461e484ed4f71f9ba5102de96b0a67
Adam Gill <30602106+Beeblesnout@users.noreply.github.com>
2019-10-31 18:29:43 -0400

    Update Rider.cs

--- DW_digital2/Assets/DWdesign2/Scripts/Rider.cs
+++ DW_digital2/Assets/DWdesign2/Scripts/Rider.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Popcron.Console;
-using UnityEngine.InputSystem;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using Timer = Beeble.Timer;
 
 public class Rider : MonoBehaviour
 {
     #region VARIABLES
 
         public int teamID;
-        public Team teamPlayer;
+        public Team team;
         public Rigidbody rb;
         public bool enableCollisions;
         public Transform knockbackPivot;
@@ -23,42 +24,54 @@ public class Rider : MonoBehaviour
             [Header("Abilities")]
             [Command("kb_str")]
             public float knockbackStrength;
-            public UnityEvent Knockback;
+            Timer knockbackCD;
+            public float knockbackCDDur;
+            public UnityEvent DoKnockback;
         #endregion
 
         #region CONTROLS
             [Header("Controls")]
-            public PlayerInput input;
+            public int cIndex;
+            public bool usingKeyboard;
             public float aimAngle;
             float smoothAimAngle;
-         
+        
             public bool queueKnockback;
         #endregion
 
     #endregion
-
-    // void Awake()
-    // {
-        
-    // }
     
-    void OnEnable() { Parser.Register(this, "buddy" + teamID); }
+    void OnEnable() { Parser.Register(this, "rider" + teamID); }
     void OnDisable() { Parser.Unregister(this); }
 
     void Start()
     {
-        teamPlayer = GetComponent<Team>();
+        team = GetComponent<Team>();
         rb = GetComponent<Rigidbody>();
         rb.detectCollisions = enableCollisions;
-        input = GetComponent<PlayerInput>();
+
+        knockbackCD = new Timer();
     }
 
     // Input Actions
-    public void OnAim(InputValue value) { aimAngle = Mathf.Atan2(value.Get<Vector2>().y, value.Get<Vector2>().x) * Mathf.Rad2Deg; }
-    public void OnKnockback() { Knockback.Invoke(); }
+    // public void OnAim(InputValue value) { aimAngle = Mathf.Atan2(value.Get<Vector2>().y, value.Get<Vector2>().x) * Mathf.Rad2Deg; }
+    // public void OnKnockback() { Knockback.Invoke(); }
 
     void Update()
     {
+        var input = Gamepad.all[cIndex].leftStick.ReadValue();
+        aimAngle = Mathf.Atan2(input.y, -input.x) * Mathf.Rad2Deg;
+
+        bool kB = false;
+        if (Gamepad.all[cIndex].buttonSouth.ReadValue() > 0) kB = true;
+        if (usingKeyboard && Keyboard.current.kKey.ReadValue() > 0) kB = true;
+        if (kB && knockbackCD.Completed) 
+        {
+            knockbackCD.Start();
+            DoKnockback.Invoke();
+        }
+
+        // Rotation
         knockbackPivot.rotation = Quaternion.AngleAxis(aimAngle - 90, Vector3.up);
         var deltaAngle = aimAngle - smoothAimAngle;
         smoothAimAngle += Mathf.Min(shieldRotateSpeed, Mathf.Abs(deltaAngle)) * Mathf.Sign(deltaAngle);
